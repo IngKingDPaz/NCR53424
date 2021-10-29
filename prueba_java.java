2b44115be7d442ceb9162f072352e2c6fae91b31
REYNER DE PAZ <ing.reyner.de.paz@gmail.com>
2021-10-29 02:04:42 +0000

    mi primer commit del proyecto 53424

---
+++ prueba_java.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class prueba_java
+{
+    
+    public static void main (String[] args) {
+        System.out.println("Hola Mundo");
+        /* code */
+    }
+    
+    
+    
+}
---
+++ prueba_js.js
@@ -0,0 +1,4 @@
+let n1=2
+let n2=3
+let suma=n1+n2
+console.log('la suma es :'+suma)
